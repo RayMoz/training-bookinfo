daa6b1140ad4bcce34944229693f345f42cad8c2
Rainer Schuppe <rs@cowoxu.eu>
2022-03-17 19:54:55 +0100

    Fixed instana setup

--- src/details/details-instana.rb
+++ src/details/details-instana.rb
@@ -17,6 +17,7 @@
 require 'webrick'
 require 'json'
 require 'net/http'
+require 'instana'
 
 if ARGV.length < 1 then
     puts "usage: #{$PROGRAM_NAME} port"
--- src/details/details.rb
+++ src/details/details.rb
@@ -17,7 +17,6 @@
 require 'webrick'
 require 'json'
 require 'net/http'
-require 'instana'
 
 if ARGV.length < 1 then
     puts "usage: #{$PROGRAM_NAME} port"
