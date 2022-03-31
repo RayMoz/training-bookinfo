31a4f514f055eb8545f17799c9d496d33166b03b
ThomasXTler <thomas@thobenamo-net.de>
2022-03-31 14:23:53 +0200

    Update ratings.js

--- src/ratings/ratings.js
+++ src/ratings/ratings.js
@@ -12,6 +12,9 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
+require('@instana/collector')();
+// added Instana agent / instrumentation hook
+
 var http = require('http')
 var dispatcher = require('httpdispatcher')
 
