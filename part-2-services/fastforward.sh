61177f49254f063a7227f6d3ee3b4d96148a61d4
Rainer Schuppe <rs@cowoxu.eu>
2022-04-21 12:43:09 +0200

    update fastfoward script

--- part-2-services/fastforward.sh
+++ part-2-services/fastforward.sh
@@ -3,6 +3,15 @@
 # Quick setup for the second part
 # This script will simple copy all the instanafied files in the right places and execute the docker build process
 # to enable the quick prep for the 2nd part of the Training
+set -o errexit
+
+if [ "$#" -ne 1 ]; then
+    echo "Incorrect parameter"
+    echo "Usage: build-services.sh <prefix>"
+    exit 1
+fi
+
+PREFIX=$1
 
 # The Ruby part
 cd ../src/details
@@ -23,7 +32,7 @@ cp ratings-instana.js ratings.js
 # build containers
 
 cd ..
-./build-service.sh
+./build-service.sh 1.0 ${PREFIX}
 
 # update docker-compose file
 cd ..
@@ -31,6 +40,4 @@ cp docker-compose-instana.yaml docker-compose.yaml
 
 # start up everything
 
-docker-compose up -d
-
-echo "Done setting up the environment. Have fun configuring the services."
+echo "Ready. Now please add ${PREFIX} into the .env file in the home directory. \nDone setting up the environment. Have fun configuring the services."
